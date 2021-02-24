0234a701f0b66e71715d8d943472023e92f0b26f
Nikolay Morgunov <talam0nal@gmail.com>
2021-02-24 13:44:51 +0300

    Imageg and description nullable

--- database/migrations/2021_02_23_082732_create_beers_table.php
+++ database/migrations/2021_02_23_082732_create_beers_table.php
@@ -17,8 +17,8 @@ class CreateBeersTable extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name');
-            $table->text('description');
-            $table->text('image');
+            $table->text('description')->nullable();
+            $table->text('image')->nullable();
         });
     }
 
